aa0aee71ada08a1a139d32e24459e50561c7ea7c
Rozenberg-Alexsander <rozenberg.s@mail.ru>
2024-02-25 16:10:43 +0300

    first

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -5,6 +5,7 @@ public abstract class ChessPiece {
     public ChessPiece(String color) {
         this.color = color;
     }
+
     public abstract String getColor();
     public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
     public abstract String getSymbol();
